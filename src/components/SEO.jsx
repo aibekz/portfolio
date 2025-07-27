541faf940dcc35d3d28da8e4607077480216af5c
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-07-26 22:00:03 -0500

    Remove react-helmet-async dependency and update SEO implementation

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet-async';
 import { siteConfig } from '../constants/siteConfig';
 import { useEffect } from 'react';
 
@@ -11,35 +10,62 @@ const SEO = ({
 }) => {
   const fullTitle = title ? `${title} | ${siteConfig.name}` : siteConfig.title;
 
-  // Force title update for React 19 compatibility
   useEffect(() => {
+    // Update document title
     document.title = fullTitle;
-  }, [fullTitle]);
 
-  return (
-    <Helmet>
-      <title>{fullTitle}</title>
-      <meta name="description" content={description} />
-      <meta name="author" content={siteConfig.name} />
-      
-      {/* Open Graph */}
-      <meta property="og:title" content={fullTitle} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:type" content={type} />
-      <meta property="og:site_name" content={siteConfig.name} />
-      {image && <meta property="og:image" content={image} />}
-      
-      {/* Twitter Card */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={fullTitle} />
-      <meta name="twitter:description" content={description} />
-      {image && <meta name="twitter:image" content={image} />}
-      
-      {/* Canonical URL */}
-      <link rel="canonical" href={url} />
-    </Helmet>
-  );
+    // Helper function to update or create meta tags
+    const updateMetaTag = (selector, content, property = 'content') => {
+      let element = document.querySelector(selector);
+      if (!element) {
+        element = document.createElement('meta');
+        if (selector.includes('property=')) {
+          element.setAttribute('property', selector.match(/property="([^"]+)"/)[1]);
+        } else if (selector.includes('name=')) {
+          element.setAttribute('name', selector.match(/name="([^"]+)"/)[1]);
+        }
+        document.head.appendChild(element);
+      }
+      element.setAttribute(property, content);
+    };
+
+    // Update meta tags
+    updateMetaTag('meta[name="description"]', description);
+    updateMetaTag('meta[name="author"]', siteConfig.name);
+    
+    // Open Graph tags
+    updateMetaTag('meta[property="og:title"]', fullTitle);
+    updateMetaTag('meta[property="og:description"]', description);
+    updateMetaTag('meta[property="og:url"]', url);
+    updateMetaTag('meta[property="og:type"]', type);
+    updateMetaTag('meta[property="og:site_name"]', siteConfig.name);
+    
+    if (image) {
+      updateMetaTag('meta[property="og:image"]', image);
+    }
+    
+    // Twitter Card tags
+    updateMetaTag('meta[name="twitter:card"]', 'summary_large_image');
+    updateMetaTag('meta[name="twitter:title"]', fullTitle);
+    updateMetaTag('meta[name="twitter:description"]', description);
+    
+    if (image) {
+      updateMetaTag('meta[name="twitter:image"]', image);
+    }
+
+    // Update canonical URL
+    let canonicalLink = document.querySelector('link[rel="canonical"]');
+    if (!canonicalLink) {
+      canonicalLink = document.createElement('link');
+      canonicalLink.setAttribute('rel', 'canonical');
+      document.head.appendChild(canonicalLink);
+    }
+    canonicalLink.setAttribute('href', url);
+
+  }, [fullTitle, description, url, image, type]);
+
+  // This component doesn't render anything
+  return null;
 };
 
 export default SEO;
