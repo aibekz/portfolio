de90cce81239ebdc9b659ee5f54aa0ba1f12c5d3
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-07-31 01:40:39 -0500

    Add theme support to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import { getCurrentYear } from '../utils';
 import { siteConfig } from '../constants/siteConfig';
+import { useTheme } from '../contexts/ThemeContext';
 
 export default function Footer() {
+  const { theme } = useTheme();
+  
   return (
     <footer className="py-10">
       <div className="flex flex-col items-center space-y-3">
-        <p className="tracking-wide text-darktext">
+        <p className={`tracking-wide ${theme === 'dark' ? 'text-gray-300' : 'text-darktext'}`}>
           © {getCurrentYear()} <span className="font-mono">{siteConfig.author.logo}</span>
         </p>
       </div>
