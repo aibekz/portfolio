568d061a05cf647e614a2c0a0f8a1173484887df
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-08-06 00:04:37 -0500

    update footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer>
-      <div className="max-w-3xl mx-auto p-6 flex items-center justify-center">
-        <p className="tracking-wide text-gray-300">
-          git commit -m "<Link to="/" className="font-mono text-sm text-accent hover:underline">Built by {siteConfig.author.logo}</Link>"
+      <div className="max-w-3xl mx-auto p-6 flex flex-col items-center justify-center space-y-4">
+        <p className="text-sm text-gray-400">
+          &copy; {new Date().getFullYear()} <Link to="/" className="font-mono text-sm text-accent hover:underline"> {siteConfig.author.name}</Link>
         </p>
       </div>
     </footer>
