3d1e2a27cf97ef34b8fbb9ac9ee0b2330dede39d
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-08-12 09:52:06 -0500

    Add domain redirects to always redirect to aibek.me and update site config URL

--- src/constants/siteConfig.js
+++ src/constants/siteConfig.js
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "Aibek Zhumabekov",
   title: "Aibek — Software Engineer ( Frontend Developer )",
   description: "Aibek is a software engineer based in Texas, USA, mainly focused on frontend development. He is passionate about building engaging, accessible, and user-centric web applications.",
-  url: "https://aibekz.com/",
+  url: "https://aibek.me/",
   linkedin: 'https://linkedin.com/in/aibekz',
   github: 'https://github.com/aibekz',
   email: "aibek.zhumabekov@outlook.com",
